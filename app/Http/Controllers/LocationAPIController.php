b491594f047622a4f5243bfa8c1f91e11d2f1ae9
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-06-09 17:17:02 +0100

    Get all countries

--- app/Http/Controllers/LocationAPIController.php
+++ app/Http/Controllers/LocationAPIController.php
@@ -1,13 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Http\Controllers;
 
-use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Client as GuzzleClient;
 
 class LocationAPIController extends Controller
 {
-	public function getCountries()
+	public function getCountries(GuzzleClient $client)
 	{
+		$res = $client->request('GET', 'http://where.yahooapis.com/v1/countries?format=json&appid=' . env('YAHOO_CLIENT_ID'));
+		
+		return $res->getBody();
 	}
 
 	public function getStates($countryName)
