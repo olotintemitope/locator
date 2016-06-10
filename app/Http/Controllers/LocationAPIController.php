b95ca1dd44a07ff7e32149e073e3f683be99f05d
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-06-10 10:42:10 +0100

    Inject the Guzzle instance into the constructor

--- app/Http/Controllers/LocationAPIController.php
+++ app/Http/Controllers/LocationAPIController.php
@@ -6,11 +6,24 @@ use GuzzleHttp\Client as GuzzleClient;
 
 class LocationAPIController extends Controller
 {
-	public function getCountries(GuzzleClient $client)
+	/**
+	 * The GuzzleClient instance to-be.
+	 */
+	protected $client;
+
+	/**
+	 * Create an instrance on Guzzle
+	 */
+	public function __construct(GuzzleClient $client)
 	{
-		$res = $client->request('GET', 'http://where.yahooapis.com/v1/countries?format=json&appid=' . env('YAHOO_CLIENT_ID'));
-		
-		return $res->getBody();
+		$this->client = $client;
+	}
+
+	public function getCountries()
+	{
+		$res = $this->client->request('GET', 'http://where.yahooapis.com/v1/countries?format=json&appid=' . env('YAHOO_CLIENT_ID'));
+
+		return json_decode( $res->getBody(), true );
 	}
 
 	public function getStates($countryName)
