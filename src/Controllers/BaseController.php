4ad7244cf4535ff7600071c1b7cd06a426f5a9b3
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-07-22 11:18:11 +0100

    Create new class for extenstion

---
+++ src/Controllers/BaseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wishi\Controllers;
+
+use Dotenv\Dotenv;
+use GuzzleHttp\Client as GuzzleClient;
+
+class BaseController
+{
+	/**
+	 * The GuzzleClient instance to-be.
+	 */
+	protected $client;
+
+	const API_URL = 'http://where.yahooapis.com/v1/';
+
+	const API_ATT = '?format=json&appid=';
+
+	/**
+	 * Create instances of Guzzle
+     * Load Environment
+	 */
+	public function __construct(GuzzleClient $client = null, Dotenv $dotenv = null)
+	{
+		$this->client = $client == null ? new GuzzleClient() : $client;
+
+		$dotenv = $dotenv == null ? new Dotenv(__DIR__ . "/../../../../") : $dotenv;
+
+		$dotenv->load();
+	}
+}
