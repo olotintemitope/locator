4fad801303f72ed07ae496f5920e8ab9c550e75c
Temitope Olotin <temitopeolotin@Temitopes-MacBook-Pro-2.local>
2016-07-25 15:32:03 +0100

    Separate base method into baseController

--- src/Controllers/Locator.php
+++ src/Controllers/Locator.php
@@ -40,45 +40,4 @@ class Locator extends BaseController
 
 		return $this->makeCollection($places, 'county');
 	}
-
-	/**
-	 * Make a collection of model instances.
-	 * 
-	 * @param  An array containing information of each location.
-	 * @param  The name of the model class.
-	 * 
-	 * @return Illuminate\Support\Collection
-	 */
-	private function makeCollection($places, $model)
-	{
-		$class = 'Wishi\Model\\' . ucwords($model);
-
-		return collect(array_map(function ($place) use($class) {
-			return new $class($place);
-		}, $places));
-	}
-
-	/**
-	 * Make a request with Guzzle Client
-	 *
-	 * @return Guzzle response
-	 */
-	private function getter ($string)
-	{
-		try {
-			return $this->client->request('GET', self::API_URL . $string . self::API_ATT . getenv('YAHOO_CLIENT_ID'));
-		} catch (Exception $e) {
-			throw RequestException::create($e->getCode());
-		}
-	}
-
-	/**
-	 * Respond with Json response.
-	 *
-	 * @return json
-	 */
-	private function respondJSON($res)
-	{
-		return json_decode( $res->getBody(), true );
-	}
 }
